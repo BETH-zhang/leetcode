ead328f2c768887deed0fbe806cc4cd0a9282ef0
BETH-zhang <522718277@qq.com>
2019-09-05 18:51:59 +0800

    添加使用双指针的方式处理

--- src/19.js
+++ src/19.js
@@ -77,4 +77,39 @@ var removeNthFromEnd = function(head, n) {
     return arr.length === 0 ? null : arr[0]
 }
 
+var removeNthFromEnd1 = function(head, n) {
+    var left = -n - 1
+    var right = 0
+    var currNode = null
+    var nextNode = null
+    while (head) {
+        left++
+        right++
+        console.log(head.val)
+        head = head.next
+    }
+    console.log(left, right)
+    right = 0
+    while (head) {
+        console.log('left: ', left, right)
+        if (right === left) {
+            console.log('left: ', left, right)
+            currNode = head
+            nextNode = head.next && head.next.next
+            console.log(currNode.val, nextNode)
+        }
+        right++
+        head = head.next
+    }
+    console.log(currNode, nextNode)
+    if (!currNode) {
+        return head
+    } else if (!nextNode) {
+        currNode.next = null
+        return head
+    }
+    currNode.next = nextNode
+
+}
+
 removeNthFromEnd(llist.head, 2)
