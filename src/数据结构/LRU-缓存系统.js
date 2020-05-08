41dec6f81c0788d23430d2a6c88db428881b3326
BETH-zhang <522718277@qq.com>
2020-05-08 15:00:00 +0800

    更新

--- src/数据结构/LRU-缓存系统.js
+++ src/数据结构/LRU-缓存系统.js
@@ -69,6 +69,7 @@ class DoubleLinkedList {
       node.setNext(oldFirst)
       this.first = node
     }
+
     this.length++
   }
 
@@ -108,6 +109,7 @@ class DoubleLinkedList {
     }
 
     node.getPrev().setNext(node.getNext())
+    
     node.setPrev(null)
     node.setNext(this.first)
     this.first = node
